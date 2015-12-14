51646e635ee29e185687ea564474149bf72eb067
root <root@tensorflow-test5.c.global-1119.internal>
2015-12-14 10:17:25 +0000

    1st Commit

---
+++ public/js/jquery.custom-file-input.js
@@ -0,0 +1,61 @@
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
+
+'use strict';
+
+;( function( $, window, document, undefined )
+{
+	$( '.inputfile' ).each( function()
+	{
+		var $input	 = $( this ),
+			$label	 = $input.next( 'label' ),
+			labelVal = $label.html();
+
+		$input.on( 'change', function( e )
+		{
+			var fileName = '';
+
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else if( e.target.value )
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				$label.find( 'span' ).html( fileName );
+			else
+				$label.html( labelVal );
+
+                        if (fileName.indexOf("jpg") !== -1 || fileName.indexOf("jpeg") !== -1) {
+var fd = new FormData($("#img")[0]);
+console.log(fd);
+$(".keyword > p").html("");
+$(".keyword > img").attr("src", "");
+$(".keyword > .loading").fadeIn();
+$.ajax({
+    url: "/photos/upload",
+    type: "POST",
+    data: fd,
+    processData: false,
+    contentType: false
+})
+.done(function( data ) {
+
+var output = data.split(")").join(")<br />");
+$(".keyword > .loading").hide();
+$(".keyword > img").attr("src", "http://104.155.201.35:3000/uploads/" +  fileName);
+$(".keyword > p").html(output);
+});
+                        } else {
+                            alert("アップロード可能なファイル形式：jpeg");
+                            $label.html( labelVal );
+                        }
+		});
+
+		// Firefox bug fix
+		$input
+		.on( 'focus', function(){ $input.addClass( 'has-focus' ); })
+		.on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
+	});
+})( jQuery, window, document );
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var multiparty = require('multiparty');
+var util = require('util');
+var exec = require('child_process').exec;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/photos/upload', function(req, res, next) {
+  var form;
+  form = new multiparty.Form();
+  return form.parse(req, function(err, fields, files) {
+    var filePath = files["file-7[]"][0]["path"];
+    var fileName = files["file-7[]"][0]["originalFilename"];
+    exec('mv ' + filePath + ' ./public/uploads/' + fileName, function(err, stdout, stderr){
+      console.log(err);
+      console.log(stdout);
+      console.log(stderr);
+      exec('python /usr/tmp/tensorflow/tensorflow/models/image/imagenet/classify_image.py --image_file /usr/tmp/node/image/public/uploads/' + fileName, function(err, stdout, stderr){
+        res.send(stdout);
+      });
+    });
+  });
+});
+
+module.exports = router;
