4b11d54a995790f5e82009f0b827374af62bf857
koheishingai <uiine.tokyo@gmail.com>
2015-12-16 03:49:09 +0900

    mongodb, ajax 修正

--- public/js/jquery.custom-file-input.js
+++ public/js/jquery.custom-file-input.js
@@ -2,69 +2,70 @@
 	By Osvaldas Valutis, www.osvaldas.info
 	Available for use under the MIT License
 */
-
 'use strict';
 
-;( function( $, window, document, undefined )
-{
-	$( '.inputfile' ).each( function()
-	{
-		var $input	 = $( this ),
-			$label	 = $input.next( 'label' ),
-			labelVal = $label.html();
+;
+(function($, window, document, undefined) {
+    $('.inputfile').each(function() {
+        var $input = $(this),
+            $label = $input.next('label'),
+            labelVal = $label.html();
 
-		$input.on( 'change', function( e )
-		{
-			var fileName = '';
+        $input.on('change', function(e) {
+            var fileName = '';
 
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-			else if( e.target.value )
-				fileName = e.target.value.split( '\\' ).pop();
+            if (this.files && this.files.length > 1)
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            else if (e.target.value)
+                fileName = e.target.value.split('\\').pop();
 
-			if( fileName )
-				$label.find( 'span' ).html( fileName );
-			else
-				$label.html( labelVal );
+            if (fileName)
+                $label.find('span').html(fileName);
+            else
+                $label.html(labelVal);
 
-                        if (fileName.indexOf("jpg") !== -1 || fileName.indexOf("jpeg") !== -1) {
-var fd = new FormData($("#img")[0]);
-console.log(fd);
-$(".keyword > p").html("");
-$(".keyword > img").attr("src", "");
-$(".keyword > .loading").fadeIn();
-$(".box.tag").hide();
-$(".box.keyword").fadeIn();
-$(".box.register").hide();
-$(".box.comment").hide();
-$.ajax({
-    url: "/photos/upload",
-    type: "POST",
-    data: fd,
-    processData: false,
-    contentType: false
-})
-.done(function( data ) {
+            if (fileName.indexOf("jpg") !== -1 || fileName.indexOf("jpeg") !== -1) {
+                var fd = new FormData($("#img")[0]);
+                console.log(fd);
+                $(".keyword > p").html("");
+                $(".keyword > img").attr("src", "");
+                $(".keyword > .loading").fadeIn();
+                $(".box.tag").hide();
+                $(".box.keyword").fadeIn();
+                $(".box.register").hide();
+                $(".box.comment").hide();
+                $.ajax({
+                        url: "/photos/upload",
+                        type: "POST",
+                        data: fd,
+                        processData: false,
+                        contentType: false
+                    })
+                    .done(function(data) {
+                        var output = data.split(")").join(")<br />");
+                        $(".keyword > .loading").hide();
+                        //$(".keyword > img").attr("src", "http://104.155.201.35:3000/uploads/" +  fileName);
+                        window.fileName = fileName;
+                        $(".keyword > img").attr("src", "http://localhost:3000/uploads/" + fileName);
+                        $(".keyword > p").html(output);
+                        $(".box.tag").fadeIn();
+                        $(".box.keyword").fadeIn();
+                        $(".box.register").fadeIn();
+                        $(".box.comment").fadeIn();
+                    });
+            } else {
+                alert("アップロード可能なファイル形式：jpeg");
+                $label.html(labelVal);
+            }
+        });
 
-var output = data.split(")").join(")<br />");
-$(".keyword > .loading").hide();
-$(".keyword > img").attr("src", "http://104.155.201.35:3000/uploads/" +  fileName);
-// $(".keyword > img").attr("src", "http://localhost:3000/uploads/" +  fileName);
-$(".keyword > p").html(output);
-$(".box.tag").fadeIn();
-$(".box.keyword").fadeIn();
-$(".box.register").fadeIn();
-$(".box.comment").fadeIn();
-});
-                        } else {
-                            alert("アップロード可能なファイル形式：jpeg");
-                            $label.html( labelVal );
-                        }
-		});
-
-		// Firefox bug fix
-		$input
-		.on( 'focus', function(){ $input.addClass( 'has-focus' ); })
-		.on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
-	});
-})( jQuery, window, document );
+        // Firefox bug fix
+        $input
+            .on('focus', function() {
+                $input.addClass('has-focus');
+            })
+            .on('blur', function() {
+                $input.removeClass('has-focus');
+            });
+    });
+})(jQuery, window, document);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var multiparty = require('multiparty');
 var util = require('util');
 var exec = require('child_process').exec;
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,13 +20,18 @@ router.post('/photos/upload', function(req, res, next) {
     var filePath = files["file-7[]"][0]["path"];
     var fileName = files["file-7[]"][0]["originalFilename"];
     exec('mv ' + filePath + ' ./public/uploads/' + fileName, function(err, stdout, stderr){
-      // res.send("test");
-      exec('python /usr/tmp/tensorflow/tensorflow/models/image/imagenet/classify_image.py --image_file /usr/tmp/node/image/public/uploads/' + fileName, function(err, stdout, stderr){
+      res.send("drumstick (score = 0.11282) academic gown, academic robe, judge's robe (score = 0.05533) mortarboard (score = 0.04146) theater curtain, theatre curtain (score = 0.03298) groom, bridegroom (score = 0.03183)");
+      /*exec('python /usr/tmp/tensorflow/tensorflow/models/image/imagenet/classify_image.py --image_file /usr/tmp/node/image/public/uploads/' + fileName, function(err, stdout, stderr){
         console.log(stdout);
         res.send(stdout);
-      });
+      });*/
     });
   });
+});
+
+router.post('/photos/update', function(req, res, next) {
+  console.log(req.body);
+  res.send('update!');
 });
 
 module.exports = router;
